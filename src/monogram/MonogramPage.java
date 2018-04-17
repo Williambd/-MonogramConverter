2a71639369654ac0f4f91b945eb95f76eabb21bb
widea9928 <widea9928@7HGM6Y1.Student.UGDSB.ED>
2018-04-17 13:23:30 -0400

    aaaaa

--- src/monogram/MonogramPage.java
+++ src/monogram/MonogramPage.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Monogram maker
+ * by williambd on 17/04/2018
+ * makes a monogram from your full name
  */
 
 package monogram;
